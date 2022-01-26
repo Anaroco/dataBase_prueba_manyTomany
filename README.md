7d32b5d78d8eb6459e2059bef51ec58092a2b76e
Anaroco <roblescortes@msn.com>
2022-01-26 08:44:08 +0100

    new repository created

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Laravel
+Laravel is accessible, powerful, and provides tools required for large, robust applications.
+
+## instalation
+In the comsole put the command:
+laravel new
+
+start your project with this basic installation and add more framework if it is needed.
+
+
+
+
---
+++ database/migrations/2022_01_25_180758_create_alumno_materia_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnoMateriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumno_materia', function (Blueprint $table) {
+            $table->id();
+           
+            $table->foreignId('alumno_id')
+            ->nullable()
+            ->constrained('alumnos');
+
+            $table->foreignId('materia_id')
+            ->nullable()
+            ->constrained('materias');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumno_materia');
+    }
+}
